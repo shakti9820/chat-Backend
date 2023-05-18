5e15689b419c8b2bdf82152320abe5d2f446130f
shakti <shaktisingh979222@gmail.com>
2023-05-18 13:39:49 +0530

    socket origin

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const server = app.listen(PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "chatty4u.netlify.app",
     credentials: true,
   },
 });
